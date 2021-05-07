c17f073d40377b972faf5b0931bba28dd47c34c1
Phillip Castillo <PhillipACastillo@gmail.com>
2021-05-07 09:24:40 -0700

    Initializr

---
+++ src/main/java/com/ascent/restfulmilktea/ResTfulMilkTeaApplication.java
@@ -0,0 +1,13 @@
+package com.ascent.restfulmilktea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulMilkTeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulMilkTeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ascent/restfulmilktea/ResTfulMilkTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascent.restfulmilktea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulMilkTeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
